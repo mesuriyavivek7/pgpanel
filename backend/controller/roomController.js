204f84db113308524e8677c32132826b9e9e94d5
rohitchauhan45 <chauhanrohith.3277@gmail.com>
2025-09-13 01:19:59 +0530

    Add authorization checks for account managers in room management functions

--- backend/controller/roomController.js
+++ backend/controller/roomController.js
@@ -1,107 +1,154 @@
+import ACCOUNT from "../models/ACCOUNT.js";
 import BRANCH from "../models/BRANCH.js";
 import ROOM from "../models/ROOM.js";
 
 
-export const createRoom = async (req, res, next) =>{
-    try{
-        const {mongoid, userType} = req
-        const {room_id, capacity, branch, remark} = req.body
+export const createRoom = async (req, res, next) => {
+    try {
+        const { mongoid, userType } = req
+        const { room_id, capacity, branch, remark } = req.body
 
-        if(!room_id || !capacity || !branch) return res.status(400).json({message:"Please provide all required fields.",success:false})
+        if (userType === 'Account') {
+            const acmanager = await ACCOUNT.findById(mongoid)
 
-        const existRoom = await ROOM.findOne({room_id,branch})
+            if (!acmanager) {
+                return res.status(404).json({ message: "Account manager not Found.", success: false })
+            }
+            if (!acmanager.branch.includes(branch)) {
+                return res.status(403).json({ message: "You are not Autherized to add Room in this Branch.", success: false })
+            }
+        }
 
-        if(existRoom) return res.status(409).json({message:"Room is already exist with given room no.",success:false})
+        if (!room_id || !capacity || !branch) return res.status(400).json({ message: "Please provide all required fields.", success: false })
+
+        const existRoom = await ROOM.findOne({ room_id, branch })
+
+        if (existRoom) return res.status(409).json({ message: "Room is already exist with given room no.", success: false })
 
         const existBranch = await BRANCH.findById(branch)
 
-        if(!existBranch) return res.status(404).json({message:"Branch not found.",success:false})
+        if (!existBranch) return res.status(404).json({ message: "Branch not found.", success: false })
 
         const newBranch = await ROOM({
             room_id,
             capacity,
             branch,
             remark,
-            added_by:mongoid,
-            added_by_type:userType
+            added_by: mongoid,
+            added_by_type: userType
         })
 
         await newBranch.save()
 
-        return res.status(201).json({message:"New room created successfully.",success:true, data:branch})
+        return res.status(201).json({ message: "New room created successfully.", success: true, data: branch })
 
-    }catch(err){
+    } catch (err) {
         next(err)
     }
 }
 
 
-export const updateRoom = async (req, res, next) =>{
-    try{
-       const {roomId} = req.params 
-       const {room_id, capacity, remark} = req.body
+export const updateRoom = async (req, res, next) => {
+    try {
+        const { roomId } = req.params
+        const { room_id, capacity, remark } = req.body
+        const { mongoid } = req
 
-      const room = await ROOM.findById(roomId)
+        const room = await ROOM.findById(roomId)
 
-      if(!room) return res.status(404).json({message:"Room not found.",success:false})
+        if (!room) return res.status(404).json({ message: "Room not found.", success: false })
 
-      if(room_id && room.room_id !== room_id){
-        const existRoom = await ROOM.findOne({room_id})
+        if (userType === 'Account') {
+            const acmanager = await ACCOUNT.findById(mongoid)
 
-        if(existRoom) return res.status(409).json({message:"Room already exist with given room no.",success:false})
+            if (!acmanager) {
+                return res.status(404).json({ message: "Account manager not Found.", success: false })
+            }
+            if (!acmanager.branch.includes(room.branch)) {
+                return res.status(403).json({ message: "You are not Autherized to Update Room in this Branch.", success: false })
+            }
+        }
 
-        room.room_id = room_id
-      }
 
-      if(capacity){
-        room.capacity = capacity
-      }
+        if (room_id && room.room_id !== room_id) {
+            const existRoom = await ROOM.findOne({ room_id })
 
-      if(remark) {
-        room.remark = remark
-      }
+            if (existRoom) return res.status(409).json({ message: "Room already exist with given room no.", success: false })
 
-      await room.save()
+            room.room_id = room_id
+        }
 
-      return res.status(200).json({message:"Room details updated successfully.",success:true, data:room})
+        if (capacity) {
+            room.capacity = capacity
+        }
 
-    }catch(err){
+        if (remark) {
+            room.remark = remark
+        }
+
+        await room.save()
+
+        return res.status(200).json({ message: "Room details updated successfully.", success: true, data: room })
+
+    } catch (err) {
         next(err)
     }
 }
 
-export const getRoomByBranchId = async (req, res, next) =>{
-    try{
-       const {branchId} = req.params
+export const getRoomByBranchId = async (req, res, next) => {
+    try {
+        const { branchId } = req.params
 
-       if(!branchId) return res.status(400).json({message:"Please provide branch id.",success:false})
+        if (!branchId) return res.status(400).json({ message: "Please provide branch id.", success: false })
 
-       const branch = await BRANCH.findById(branchId)
+        if (userType === 'Account') {
+            const acmanager = await ACCOUNT.findById(mongoid)
 
-       if(!branch) return res.status(404).json({message:"Branch not found.",success:false})
+            if (!acmanager) {
+                return res.status(404).json({ message: "Account manager not Found.", success: false })
+            }
+            if (!acmanager.branch.includes(branchId)) {
+                return res.status(403).json({ message: "You are not Autherized to add Room in this Branch.", success: false })
+            }
+        }
 
-       const rooms = await ROOM.find({branch:branchId}).populate('branch')
+        const branch = await BRANCH.findById(branchId)
 
-       return res.status(200).json({message:"Rooms retrived successfuly.",success:false,data:rooms})
+        if (!branch) return res.status(404).json({ message: "Branch not found.", success: false })
 
-    }catch(err){
+        const rooms = await ROOM.find({ branch: branchId }).populate('branch')
+
+        return res.status(200).json({ message: "Rooms retrived successfuly.", success: false, data: rooms })
+
+    } catch (err) {
         next(err)
     }
 }
 
-export const getRoomById = async (req, res, next) =>{
-    try{
-       const {roomId} = req.params
+export const getRoomById = async (req, res, next) => {
+    try {
+        const { roomId } = req.params
 
-       if(!roomId) return res.status(400).json({message:"Please provide room id.",success:false})
+        if (!roomId) return res.status(400).json({ message: "Please provide room id.", success: false })
 
-       const room = await ROOM.findById(roomId).populate('branch')
+        const room = await ROOM.findById(roomId).populate('branch')
 
-       if(!room) return res.status(404).json({message:"Room not found.",success:false})
+        if (!room) return res.status(404).json({ message: "Room not found.", success: false })
 
-       return res.status(200).json({message:"Room details retrived successfully.",success:true,data:room})
+        if (userType === 'Account') {
+            const acmanager = await ACCOUNT.findById(mongoid)
 
-    }catch(err){
+            if (!acmanager) {
+                return res.status(404).json({ message: "Account manager not Found.", success: false })
+            }
+            if (!acmanager.branch.includes(room.branch)) {
+                return res.status(403).json({ message: "You are not Autherized to add Room in this Branch.", success: false })
+            }
+        }
+
+        return res.status(200).json({ message: "Room details retrived successfully.", success: true, data: room })
+
+    } catch (err) {
         next(err)
     }
 }
